81d49c5888354b062d3df097ee9cb833d1e2632c
siddh4489 <siddh4489@gmail.com>
2018-03-28 23:19:16 +0530

    Update task.js

--- server/task.js
+++ server/task.js
@@ -48,47 +48,16 @@ function managerList(req, res, next) {
 
 
 function createTask(req, res, next) {
-
- db.query('SELECT sfid FROM salesforce.contact WHERE id=$1',[req.userId], true)
-        .then(function (user) {
-            console.log(req.userId+"---------sfid------------------>: " + user.sfid);
-            // case is a reserved word. using _case instead.
-            var claimObj = nforce.createSObject('Claim__c');
-            claimObj.set('Claimant_Name__c', req.body.claimant);
-            claimObj.set('Communication_Address__c', req.body.address);
-            claimObj.set('PAN_Number__c', req.body.panno);
-            claimObj.set('Policy_Holder_Name__c', req.body.policyholdername);
-            claimObj.set('Telephone_Number__c', req.body.phone);
-            claimObj.set('Linked_Contact__c', req.userId);
-            
-
-            org.insert({ sobject: claimObj}, function(err, resp){
-                if (err) {
-                    console.log('First case insert failed: ' + JSON.stringify(err));
-                    org.authenticate({username: userName, password: password}, function(err) {
-                        if (err) {
-                            console.log('Authentication failed: ' + JSON.stringify(err));
-                            return next(err);
-                        } else {
-                            // retry
-                            org.insert({ sobject: claimObj}, function(err, resp) {
-                                if (err) {
-                                    console.log('Second case insert failed: ' + JSON.stringify(err));
-                                    return next(err);
-                                } else {
-                                    console.log('Second case insert worked');
-                                    return res.send('ok');
-                                }
-                            });
-                        }
-                    })
-                } else {
-                    console.log('First case insert worked');
-                    res.send('ok');
-                }
-            });
-        })
-.catch(next);
+    console.log('---createTask name --->'+req.body.name);
+    console.log('---createTask desc --->'+req.body.desc);
+    console.log('---createTask hours--->'+req.body.hours);
+    console.log('---createTask managerid--->'+req.body.managerid);
+    console.log('---createTask projecttype--->'+req.body.projecttype);
+    
+    console.log('---createTask 1--->'+req.body.spassword);
+    console.log('---createTask 2--->'+req.body.suser);
+    console.log('---createTask 3 --->'+JSON.stringify(req.body));
+ 
     
 };
 
