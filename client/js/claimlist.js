62dcdc0a12f1f6ba51249b378d7f30deba9fd338
siddh4489 <siddh4489@gmail.com>
2018-03-28 15:57:46 +0530

    Update claimlist.js

--- client/js/claimlist.js
+++ client/js/claimlist.js
@@ -25,14 +25,9 @@ angular.module('nibs.claimlist', ['nibs.config'])
 
     //Controllers
     .controller('ClaimListController', function ($scope, $window, $ionicPopup, Claimlist, User) {
-        //var data = $.param({'sfuser':$window.localStorage.getItem('sfuser'),'sfpassword':$window.localStorage.getItem('sfpassword')});
         $scope.claimlist = {};
-        $scope.sfu = {'sf':$window.localStorage.getItem('sfuser'),'pf':$window.localStorage.getItem('sfpassword')};
+        $scope.sfu = {'suser':$window.localStorage.getItem('sfuser'),'spassword':$window.localStorage.getItem('sfpassword')};
             Claimlist.getClaimList($scope.sfu).success(function(datalist) {
                      $scope.claimlist = datalist;
                 });
-      
-
-       
-
   });
