ba9c00d0e66297e8010171aff672119f4a475c25
siddh4489 <siddh4489@gmail.com>
2018-03-29 15:26:44 +0530

    Update task.js

--- client/js/task.js
+++ client/js/task.js
@@ -73,13 +73,13 @@ angular.module('nibs.task', ['nibs.config'])
                         }
                     });
 
-    
+       $scope.task = {managerid: mId};
        $scope.submit = function () {
                   alert(' $scope.task--'+$scope.task);
                   alert(' $scope.task Json--'+JSON.stringify($scope.task));
-                  $scope.task = {managerid: mId};                            
+                  //$scope.task = {managerid: mId};                            
                   //$scope.task = {managerid: mId,projecttype:pName,sfu: $window.localStorage.getItem('sfuser'),spassword: $window.localStorage.getItem('sfpassword')};
-                  alert(' $scope.task Json 2--'+JSON.stringify($scope.task));
+                  //alert(' $scope.task Json 2--'+JSON.stringify($scope.task));
                 //$scope.task = {sfu: $window.localStorage.getItem('sfuser')};
                 //$scope.task = {sfu: $window.localStorage.getItem('spassword')};
                 //alert(JSON.strigify($scope.task));
