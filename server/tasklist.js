64dbd11498b3761673fd702c923578d8f3521a71
siddh4489 <siddh4489@gmail.com>
2018-03-30 12:25:55 +0530

    Update tasklist.js

--- server/tasklist.js
+++ server/tasklist.js
@@ -18,8 +18,9 @@ function getTasklists(req, res, next) {
     org.authenticate({ username: req.body.suser, password: req.body.spassword}, function(err, resp) {
         if(!err) {
         console.log(' Logged in user id : '+req.body.uid);   
-        var q = "SELECT Id,Task_Name__c,Task_Description__c,Project_Type__c,No_of_Hours__c,Manager_Name__c FROM Task__c where SELECT Id,Task_Name__c,Task_Description__c,Project_Type__c,No_of_Hours__c,Manager_Name__c,Task_Date__c FROM Task__c where Task_Date__c = 2018-03-29 AND createdbyId ='"+req.body.uid+"'";
-        org.query({ query: q }, function(err, resp){
+        var q = "SELECT Id,Task_Name__c,Task_Description__c,Project_Type__c,No_of_Hours__c,Manager_Name__c FROM Task__c where SELECT Id,Task_Name__c,Task_Description__c,Project_Type__c,No_of_Hours__c,Manager_Name__c,Task_Date__c FROM Task__c where Task_Date__c = '2018-03-29' AND createdbyId ='"+req.body.uid+"'";
+        console.org('----q---'+q);
+            org.query({ query: q }, function(err, resp){
               if(!err && resp.records) {
                  console.log(' resp.records in user id : '+resp.records); 
                  res.send(resp.records);
