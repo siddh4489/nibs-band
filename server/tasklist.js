5a68ede30c8caf4b6016ec6dbb4f199545b5f1f1
siddh4489 <siddh4489@gmail.com>
2018-03-30 12:45:36 +0530

    Update tasklist.js

--- server/tasklist.js
+++ server/tasklist.js
@@ -37,6 +37,44 @@ function getTasklists(req, res, next) {
      
 };
 
+
+function getResourceview(req, res, next) {
+   
+
+    var oauth;
+     org = nforce.createConnection({
+            clientId: config.api.clientId,
+            clientSecret: config.api.clientSecret,
+            redirectUri: config.api.redirectUri,
+            apiVersion: config.api.apiVersion,  // optional, defaults to current salesforce API version
+            environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+            mode: 'single' // optional, 'single' or 'multi' user mode, multi default
+        });
+
+    org.authenticate({ username: req.body.suser, password: req.body.spassword}, function(err, resp) {
+        if(!err) {
+        console.log(' Logged in user id : '+req.body.uid);   
+        var q = "SELECT Id,Task_Name__c,Task_Description__c,Project_Type__c,No_of_Hours__c,Manager_Name__c FROM Task__c where Task_Date__c = 2018-03-29 ";
+            org.query({ query: q }, function(err, resp){
+              if(!err && resp.records) {
+                 console.log(' resp.records in user id : '+resp.records); 
+                 res.send(resp.records);
+              }else{
+                  console.log(' resp. no records: ');
+                 res.send('No record Available');
+              }
+        });
+        } else {
+            console.log('nforce connection failed: ' + err.message);
+            oauth = resp;
+        }
+    });
+     
+};
+
+
+
+
 function revokeToken(req, res, next) {
     org.revokeToken({token: org.oauth.access_token}, function(err) {
         if (err) {
@@ -48,5 +86,6 @@ function revokeToken(req, res, next) {
 
 }
 
+exports.getResourceview = getResourceview;
 exports.getTasklists = getTasklists;
 exports.revokeToken = revokeToken;
